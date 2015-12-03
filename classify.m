e29d23aa3586f190079674fa3d12b455245c1289
Clayton Ritcher <claytonritcher@gmail.com>
2015-12-03 01:13:20 -0500

    Added weighting to knn. Now gets 54.1% on autolab

--- classify.m
+++ classify.m
@@ -1,7 +1,11 @@
 function Y = classify(m, X)
   Y = zeros(size(X, 1), 1);
+  % For each data point, extract its features and use k-nearest neighbors to 
+  % classify it.
+  t = time();
   for ind = 1:size(X,1)
     transformed = extract_feature(X(ind, :), m.principleComps);
-    Y(ind, 1) = knn(transformed, m.clusters, m.labels, 20);
+    Y(ind, 1) = knn(transformed, m.clusters, m.labels, 50, .9);
   end
+  (time() - t) / size(X, 1)
 end
--- knn.m
+++ knn.m
@@ -1,9 +1,26 @@
-function class = knn(point, clusters, labels, k)
+function class = knn(point, clusters, labels, k, weighting)
+% KNN Classify using weighted k-nearest neighbors
+%   knn(point, clusters, labels, k, weighting)
+%   point - the test point to classify
+%   clusters - the training points (possible neighbors)
+%   labels - the associated labels for each point in clusters
+%   k - how many closest neighbors to use for classification
+%   weighting - how much to favor the votes of closer neighbors
+
   % Create array of distances
   repeatedPoint = repmat(point, [size(clusters, 1), 1]);
   dists = sum((repeatedPoint .- clusters).^2, 2);
   % Sort the array and grab the top k
   [~, kIndices] = sort(dists);
+  relevantDists = dists(kIndices(1:k));
   neighborLabels = labels(kIndices(1:k));
-  class = mode(neighborLabels);
+  % weight the votes
+  weights = (ones(k, 1) .- (relevantDists ./ max(relevantDists))) .^ weighting;
+  votes = zeros(1, k);
+  for ind = 0:max(labels)
+    votes(1,ind + 1) = sum(weights(neighborLabels == ind));
+  end
+  % return the class with the most votes  
+  [~,class] = max(votes);
+  class -= 1;
 end
